973b314018f71e24d72e2f2a6aeb7f1e2f60f8f6
karthedew <karthedew@gmail.com>
2022-02-25 22:58:56 -0800

    Added updates for initial NFID contract.

---
+++ truffle/migrations/1_deploy_atlanticid.js
@@ -0,0 +1,88 @@
+const NfidContract = artifacts.require('AtlanticId')
+const DateTimeLibrary = artifacts.require('DateTime')
+
+var fs = require('fs')
+
+
+module.exports = async function(deployer, network, accounts) {
+    console.log('---------------------------------')
+    console.log('NETWORK: ')
+    console.log(network)
+    console.log('---------------------------------')
+    console.log('ACCOUNTS: ')
+    console.log(accounts)
+    console.log('---------------------------------')
+    console.log('DEPLOYER: ')
+    // console.log(deployer)
+    console.log('---------------------------------')
+
+    // Check if 'outputs' directory
+    fs.access("outputs", function(error) {
+        if (error) {
+          console.log("Creating *outputs* directory...")
+          fs.mkdirSync("outputs")
+        }
+    })
+
+    if (network == 'ganache') {
+        console.log('=========================================')
+        console.log(`|   Welcome to the ${network} network   |`)
+        console.log('=========================================')
+
+        /** DEPLOY CONTRACT(S) */
+        await deployer.deploy(DateTimeLibrary)
+        await deployer.link(DateTimeLibrary, NfidContract)
+        await deployer.deploy(NfidContract)
+        /** ------------------ */
+        const instance = await NfidContract.deployed()
+
+        let data = {
+            "owner": accounts[0],
+            "contract_address": instance.address,
+            "network": network,
+            "addresses": accounts
+        }
+
+        var jsonData = JSON.stringify(data);
+        fs.writeFileSync("outputs/1_ganacheCli_NfidContract.json", jsonData, function(err) {
+            if (err) {
+                console.log(err);
+            }
+        })
+    }
+
+    else if (network == 'ganacheCli') {
+        console.log('=========================================')
+        console.log(`|   Welcome to the ${network} network   |`)
+        console.log('=========================================')
+
+        await deployer.deploy(NfidContract)
+        const instance = await NfidContract.deployed()
+
+        let data = {
+            "owner": accounts[0],
+            "contract_address": instance.address,
+            "network": network,
+            "addresses": accounts
+        }
+
+        var jsonData = JSON.stringify(data);
+        fs.writeFileSync("outputs/1_ganacheCli_NfidContract.json", jsonData, function(err) {
+            if (err) {
+                console.log(err);
+            }
+        })
+    }
+
+    else if (network == 'kovan') {
+        console.log('This is the local host')
+    }
+
+    else if (network == 'mainnet') {
+        console.log('This is the local host')
+    }
+
+    else {
+        console.log(`Network specified (${network}) not available...`)
+    }
+}
--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -1,4 +1,89 @@
+require("dotenv").config()
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+
+
 module.exports = {
+  // Uncommenting the defaults below 
+  // provides for an easier quick-start with Ganache.
+  // You can also follow this format for other networks;
+  // see <http://truffleframework.com/docs/advanced/configuration>
+  // for more details on how to specify configuration options!
+  // 000000000000
+  networks: {
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+      websockets: true,
+      gas: 4712388,
+      gasPrice: 1000000000000
+    },
+    ganacheCli: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      websockets: true,
+      gas: 30000000,
+      gasPrice: 2000000000
+    },
+    develop: {
+      host: "127.0.0.1",
+      port: 9545,
+      network_id: "*",
+      gas: 4712388000000000000000000000000,
+      gasPrice: 10000000
+    },
+    kovan: {
+      provider: function() {
+        // Can replace the Private Key with either a MNEMONIC or list of [ Private Key #1, Private Key #2 ].
+        return new HDWalletProvider(process.env.KOVAN_PRIVATE_KEY, process.env.KOVAN_HTTP_URL)
+    },
+    network_id: 42,
+    gasPrice: 100000000000, // 20 GWEI
+    gas: 4712388,          // gas limit, set any number you want,
+    skipDryRun: true
+   },
+   bsc_testnet: {
+    provider: () => new HDWalletProvider(process.env.BSC_TESTNET_PRIVATE_KEY, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+    network_id: 97,
+    confirmations: 10,
+    timeoutBlocks: 200,
+    skipDryRun: true
+  },
+  bsc: {
+    provider: () => new HDWalletProvider(process.env.BSC_PRIVATE_KEY, `https://bsc-dataseed1.binance.org`),
+    network_id: 56,
+    confirmations: 10,
+    timeoutBlocks: 200,
+    skipDryRun: true
+  },
+  },
+  // contracts_directory: './contracts',
+  contracts_build_directory: '../frontend/src/app/artifacts/abis',
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    timeout: 100000,
+    useColors: true
+  },
+  compilers: {
+    solc: {
+      version: "0.8.11",                                    // Fetch exact version from solc-bin (default: truffle's version)
+      docker: false,                                        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {                                           // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 200
+       },
+       evmVersion: "byzantium"
+      }
+    },
+  },
+  //
+};
+
+
+
+// module.exports = {
   // Uncommenting the defaults below 
   // provides for an easier quick-start with Ganache.
   // You can also follow this format for other networks;
@@ -38,4 +123,4 @@ module.exports = {
     //   }
     // }
   // }
-};
+// };
