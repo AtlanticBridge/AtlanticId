f16a3bd2cb47e740b53a8dc49d0aeb5196bc1f89
Harry Shuttleworth <hshuttleworth0@gmail.com>
2022-03-07 21:53:04 +0000

    Set the default working directory

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = var.ACCESS_KEY
-  secret_key = var.SECRET_KEY
+  region = "us-east-1"
+  # access_key = var.ACCESS_KEY
+  # secret_key = var.SECRET_KEY
 }
 
 terraform {
